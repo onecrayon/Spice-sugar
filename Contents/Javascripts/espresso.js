3bb402b211c093ae62577be80dd53460a62d13e9
Thomas Aylott <oblivious+git@subtlegradient.com>
2009-07-14 22:26:51 -0400

    added attribution

--- Contents/Javascripts/espresso.js
+++ Contents/Javascripts/espresso.js
@@ -1,4 +1,8 @@
 // Utility methods for working with Espresso
+// Created by:
+//     Ian Beck / OneCrayon -- http://beckism.com/
+//     Thomas Aylott / SubtleGradient -- http://subtlegradient.com/
+// MIT License
 
 importJS('mootools-server');
 
