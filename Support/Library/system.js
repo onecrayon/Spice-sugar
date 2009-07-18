f54e1f10872a29001a2ef96463d65647bb48d6b7
Ian Beck <ian@onecrayon.com>
2009-07-17 19:54:54 -0700

    Modified importJS to use new findScripts function

--- Support/Library/system.js
+++ Support/Library/system.js
@@ -1,5 +1,13 @@
-function importJS(file) {
-	file = JSCocoaLoaderController.findScript_(file);
+// This function is going to die very, very soon
+// Merely here to provide a way to load scripts while testing require()
+function importJS(file, searchScripts) {
+	var searchScripts = (typeof searchScripts !== 'undefined' ? searchScripts : false);
+	if (searchScripts) {
+		var folders = new Array('Scripts', 'Library');
+	} else {
+		var folders = new Array('Library');
+	}
+	file = JSCocoaLoaderController.findScript_inFolders_(file, folders);
 	if (file == null) throw new Error("Cannot find the file '"+file+"'");
 	else __jsc__.evalJSFile(file);
 }
