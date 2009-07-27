c3e00d998f435587ff7fa543df40b762f093876f
Ian Beck <ian@onecrayon.com>
2009-07-26 20:24:02 -0700

    Added basic text recipe stub file

---
+++ Support/Library/text_recipe.js
@@ -0,0 +1,15 @@
+// Class for creating and executing text recipes
+// Sets up the modular environment to enable scripts and helper modules
+//
+// Created by:
+//     Ian Beck / OneCrayon -- http://onecrayon.com/
+//     Thomas Aylott / SubtleGradient -- http://subtlegradient.com/
+// MIT License
+
+require.global('mootools-server');
+
+var TextRecipe = new Class({
+	
+});
+
+exports.TextRecipe = TextRecipe;
