f5bcb7d495c69dfbf71624880ecdf91e8a89ea80
Ian Beck <ian@onecrayon.com>
2009-07-27 08:38:02 -0700

    Added basic text recipe logic and testing action

--- JSCocoaExample.sugar/Support/Scripts/hello_world.js
+++ JSCocoaExample.sugar/Support/Scripts/hello_world.js
@@ -1,6 +1,11 @@
 var Snippet = require('snippet');
+var textContext = require('text_action_context').textContext;
+var Recipe = require('text_recipe').TextRecipe;
 
 exports.main = function(snippet) {
+	var recipe = new Recipe('Hello World!');
+	var selections = textContext.getSelections();
+	recipe.remove(selections).apply();
 	snippet.toSnippet().write();
 	return true;
 }
--- Support/Library/text_recipe.js
+++ Support/Library/text_recipe.js
@@ -7,9 +7,76 @@
 // MIT License
 
 require.global('mootools-server');
+var Range = require('range').Range;
+
+function arrayWithRanges(ranges) {
+	// Utility function for converting a single range into an array of ranges
+	// Where does this belong? Stuck it here because it was where I was using it,
+	// but should probably go somewhere more useful and be required in
+	if ($type(ranges) !== 'array') {
+		ranges = [ranges];
+	}
+	return ranges;
+}
 
 var TextRecipe = new Class({
+	initialize: function(undo_name) {
+		this.prepared = false;
+		this.recipe = CETextRecipe.textRecipe;
+		if ($type(undo_name) === 'string') {
+			this.recipe.undoActionName = undo_name;
+		}
+	},
 	
+	insert: function(string, rangeOrIndex) {
+		var index;
+		if ($type(rangeOrIndex) === 'range') {
+			index = rangeOrIndex.location;
+		} else if ($type(rangeOrIndex) === 'number') {
+			index = rangeOrIndex;
+		} else {
+			rangeOrIndex = new Range(rangeOrIndex);
+			index = rangeOrIndex.location;
+		}
+		this.recipe.addInsertedString_forIndex_(string, index);
+	},
+	
+	replace: function(string, ranges) {
+		ranges = arrayWithRanges(ranges);
+		ranges.each(function(range) {
+			if ($type(range) !== 'range') {
+				range = new Range(range);
+			}
+			this.recipe.addReplacementString_forRange_(string, range.rangeValue());
+		});
+	},
+	
+	remove: function(ranges) {
+		ranges = arrayWithRanges(ranges);
+		ranges.each(function(range) {
+			if ($type(range) !== 'range') {
+				range = new Range(range);
+			}
+			this.recipe.addDeletedRange_(range.rangeValue());
+		});
+	},
+	
+	prepare: function() {
+		// Shortcut for preparing the recipe
+		this.prepared = true;
+		this.recipe.prepare;
+	},
+	
+	numberOfChanges: function() {
+		if (!this.prepared) {
+			this.prepare();
+		}
+		return this.recipe.numberOfChanges;
+	},
+	
+	apply: function() {
+		return context.applyTextRecipe_(this.recipe);
+	}
 });
 
 exports.TextRecipe = TextRecipe;
